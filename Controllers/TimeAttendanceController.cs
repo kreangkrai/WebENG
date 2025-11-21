e30c323bb4fbd49724dd6ccf72a9d445188f487a
sarit-t <sarit_t@contrologic.co.th>
2025-11-21 17:30:47 +0700

    update add Time Attendance

---
+++ Controllers/TimeAttendanceController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WebENG.Interface;
+using WebENG.Models;
+using WebENG.Service;
+
+namespace WebENG.Controllers
+{
+    public class TimeAttendanceController : Controller
+    {
+        private readonly IAccessory Accessory;
+
+        public TimeAttendanceController()
+        {
+            Accessory = new AccessoryService();
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetString("Login_ENG") != null)
+            {
+                string user = HttpContext.Session.GetString("userId");
+                List<UserModel> users = new List<UserModel>();
+                users = Accessory.getAllUser();
+                UserModel u = users.Where(w => w.name.ToLower() == user.ToLower()).Select(s => new UserModel
+                {
+                    name = s.name,
+                    department = s.department,
+                    role = s.role,
+                    user_id = s.user_id,
+                    emp_id = s.emp_id
+                }).FirstOrDefault();
+                HttpContext.Session.SetString("Name", u.name);
+                HttpContext.Session.SetString("Department", u.department);
+                HttpContext.Session.SetString("Role", u.role);
+
+                return View(u);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Account");
+            }
+        }
+    }
+}
